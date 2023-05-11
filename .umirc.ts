9806c9d1fd332e4b8b77fa2a7e9b827c069e6a87
Tuwan <ping.zhang@xuncetech.com>
2023-05-11 17:42:52 +0800

    fix: 优化

--- .umirc.ts
+++ .umirc.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "umi";
 export default defineConfig({
   routes: [
     { path: "/", component: "index" },
-    { path: "/demo_01", component: "demo_01" },
+    { path: "/formSmallFile", component: "formSmallFile" },
     { path: "/test01", component: "test01" },
   ],
   npmClient: 'yarn'
